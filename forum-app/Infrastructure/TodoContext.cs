1ab74e46d2833f38788c2caec068f9f9cf8e2ad2
versave <vszhivkov@gmail.com>
2022-12-10 11:15:29 +0200

    Add base files

---
+++ forum-app/Infrastructure/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using forum_app.Model;
+
+namespace forum_app.Infrastructure {
+    public class TodoContext: DbContext {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoList> todoList { get; set; }
+    }
+}
---
+++ forum-app/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using forum_app.Infrastructure;
+using forum_app.Model;
+
+namespace forum_app.Controllers {
+    public class IndexModel : PageModel {
+        private readonly TodoContext _context;
+
+        public IndexModel(TodoContext context) {
+            _context = context;
+        }
+
+        public IList<TodoList> TodoList { get;set; }
+
+        public async Task OnGetAsync() {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            TodoList = await _context.todoList.Where(todo => todo.UserId == userId).ToListAsync();
+        }
+    }
+}
